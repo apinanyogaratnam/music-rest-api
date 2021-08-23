745b3047b1f2553f6477d44698ed3ab666826050
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-23 17:49:58 -0400

    completed all http requests and updated readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# Music API
+
+# Description
+An API to save your music titles in a database and retrieve them on demand.
+
+# Usage
+Base URL: `/api/v1/posts`
+
+### Get All Music
+`GET/`
+
+### Add a song
+`POST/`
+
+### Get a specific song
+`GET/:id`
+
+### Delete a song
+`DELETE/:id`
+
+### Update a song
+`PUT/:id`
--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ const router = express.Router();
 // import { getPosts, createPost } from '../controllers/posts.js';
 import Post from '../models/Post.js';
 
-
+// get all posts
 router.get('/', async (req, res) => {
     try {
         const posts = await Post.find();
@@ -13,6 +13,7 @@ router.get('/', async (req, res) => {
     }
 });
 
+// create a post
 router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title
@@ -21,6 +22,39 @@ router.post('/', async (req, res) => {
     try {
         const savedPost = await post.save();
         res.json(savedPost);
+    } catch (error) {
+        res.json({ message: error });
+    }
+});
+
+// get a specific post
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch (error) {
+        res.json({ message: error });
+    }
+});
+
+// delete a post
+router.delete('/postId', async (req, res) => {
+    try {
+        const removedPost = await Post.remove({ _id: req.params.postId });
+        res.json(removedPost);
+    } catch (error) {
+        res.json({ message: error });
+    }
+});
+
+// update a post
+router.patch('/:postId', async (req, res) => {
+    try {
+        const updatedPost = await Post.updateOne(
+            { _id: req.params.postId },
+            { $set: { title: req.body.title } }
+        );
+        res.json(updatedPost);
     } catch (error) {
         res.json({ message: error });
     }
