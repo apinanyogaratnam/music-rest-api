9fdaca1b79498d4dcc070094b09185f4c8280874
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-23 16:57:15 -0400

    connected to db

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Home page");
+});
+
+mongoose.connect(
+    process.env.DATABASE_URL, 
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log('Connected to database')
+);
+
+app.listen(3000, () => console.log("server started on port 3000"));
